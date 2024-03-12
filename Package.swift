12c969b5d7c4f32863b6ef0e9dff03d18f98d64c
deepakrajm <deepakraj.m@shield.com>
2024-03-12 15:27:31 +0530

    package swift is modified with fraud as dependency

--- Package.swift
+++ Package.swift
@@ -1,20 +1,20 @@
-// swift-tools-version: 5.10
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+
 let version = "1-0-0"
 let shieldbehavioural_checksum = "9c543086a65514b4249d32b1b56f4942eadf1cbd383947889d345b1cffd18e62"
-
 
 let package = Package(
     name: "shield-behavioural-analysis-spm",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v12) // Adjust deployment target if needed
     ],
     products: [
         .library(
             name: "BehaviouralAnalysis",
-            targets: ["BehaviouralAnalysis"]
+            targets: ["BehaviouralAnalysisTarget"]
         )
     ],
     dependencies: [
@@ -25,6 +25,13 @@ let package = Package(
             name: "BehaviouralAnalysis",
             url: "https://s3.amazonaws.com/cashshield-sdk/shieldbehavioural-ios-swift-\(version).zip",
             checksum: shieldbehavioural_checksum
-        )
+        ),
+        .target(
+            name: "BehaviouralAnalysisTarget",
+            dependencies: [
+              .target(name: "BehaviouralAnalysis"),
+              .product(name: "ShieldFraud", package: "shieldfraud-spm")
+            ]
+          )
     ]
 )
