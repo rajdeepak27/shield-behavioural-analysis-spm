0ac3ff13eb4f3cb9cbe2e04c397d5e0ca14253b6
deepakrajm <deepakraj.m@shield.com>
2024-03-12 14:59:45 +0530

    added package dependency

--- Package.swift
+++ Package.swift
@@ -9,19 +9,22 @@ let shieldbehavioural_checksum = "9c543086a65514b4249d32b1b56f4942eadf1cbd383947
 let package = Package(
     name: "shield-behavioural-analysis-spm",
     platforms: [
-           .iOS(.v12)
-       ],
+        .iOS(.v12)
+    ],
     products: [
-            .library(
-                name: "BehaviouralAnalysis",
-                targets: ["BehaviouralAnalysis"]
-            )
-        ],
-        targets: [
-            .binaryTarget(
-                name: "BehaviouralAnalysis",
-                url: "https://s3.amazonaws.com/cashshield-sdk/shieldbehavioural-ios-swift-\(version).zip",
-                checksum: shieldbehavioural_checksum
-            )
-        ]
+        .library(
+            name: "BehaviouralAnalysis",
+            targets: ["BehaviouralAnalysis"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/shield-ai-technology/shieldfraud-spm.git", from: "1.5.36")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BehaviouralAnalysis",
+            url: "https://s3.amazonaws.com/cashshield-sdk/shieldbehavioural-ios-swift-\(version).zip",
+            checksum: shieldbehavioural_checksum
+        )
+    ]
 )
